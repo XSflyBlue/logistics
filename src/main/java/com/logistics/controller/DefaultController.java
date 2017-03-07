009ae16360fa0e0267078806b38416973dbbf8a2
cappuccinoqi <617674934@qq.com>
2017-03-07 15:41:02 +0800

    update jsp folder

---
+++ src/main/java/com/logistics/controller/DefaultController.java
@@ -0,0 +1,11 @@
+package com.logistics.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+@Controller
+public class DefaultController {
+	@RequestMapping("/index")
+	public String index () {
+		return "index"; 		
+	}
+}
